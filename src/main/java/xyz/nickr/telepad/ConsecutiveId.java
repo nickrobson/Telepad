8b7797f338ca8ef2125edca939f9741bc5381fbc
Nick Robson <robson.nicholas1@gmail.com>
2016-06-09 15:42:06 +1000

    Fix ConsecutiveId

--- src/main/java/xyz/nickr/telepad/ConsecutiveId.java
+++ src/main/java/xyz/nickr/telepad/ConsecutiveId.java
@@ -11,13 +11,13 @@ public class ConsecutiveId {
     private static List<Character> chars = new LinkedList<>();
 
     static {
-        for (char i = 'a', j = 'z'; i < j; i++) {
+        for (char i = 'a', j = 'z'; i <= j; i++) {
             ConsecutiveId.chars.add(i);
         }
-        for (char i = 'A', j = 'Z'; i < j; i++) {
+        for (char i = 'A', j = 'Z'; i <= j; i++) {
             ConsecutiveId.chars.add(i);
         }
-        for (char i = '0', j = '9'; i < j; i++) {
+        for (char i = '0', j = '9'; i <= j; i++) {
             ConsecutiveId.chars.add(i);
         }
     }
@@ -28,14 +28,14 @@ public class ConsecutiveId {
 
     public static String next(String namespace) {
         if (!ConsecutiveId.store.containsKey(namespace)) {
-            return ConsecutiveId.store.computeIfAbsent(namespace, n -> "a");
+            return ConsecutiveId.store.computeIfAbsent(namespace, n -> chars.get(0).toString());
         }
         String curr = ConsecutiveId.store.get(namespace);
         int idx = ConsecutiveId.chars.indexOf(curr.charAt(curr.length() - 1));
         if (idx == -1) {
             throw new IllegalStateException("char '" + curr.charAt(curr.length() - 1) + "' is not in chars list");
         } else if (idx == ConsecutiveId.chars.size() - 1) {
-            curr += "a";
+            curr += chars.get(0);
         } else {
             curr = curr.substring(0, curr.length() - 1) + ConsecutiveId.chars.get(idx + 1);
         }
